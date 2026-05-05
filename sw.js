fdbe7498a273e8e4036747dba9bc6928f2660275
Saturdatfactor <66202303038@pi.ac.th>
2026-05-05 14:47:32 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'dental-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// ติดตั้ง Service Worker และ Caching ไฟล์
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// ดึงข้อมูลจาก Cache มาใช้ตอน Offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
